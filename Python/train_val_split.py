e092e9d7cc377d856968838212bd06dc32c5674e
Park, Se Hyeon <152643439+UBDBD@users.noreply.github.com>
2025-07-11 20:36:14 +0900

    Delete Python directory

--- Python/train_val_split.py
+++
@@ -1,6 +0,0 @@
-import splitfolders
-
-input_folder = "/content/drive/MyDrive/valid"
-output_folder = "/content/drive/MyDrive/valid_split"
-
-splitfolders.ratio(input_folder, output=output_folder, seed=1337, ratio=(0.8, 0.2), group_prefix=None)
